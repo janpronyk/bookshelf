09fce9dfdd18c50c6f4338304c2105c341f5d98c
Jan Pronyk <janpronyk@imac-jan.home>
2019-09-16 11:49:53 +0200

    server: book routes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express()
 
 // Database
 mongoose.Promise = global.Promise
-mongoose.connect(config.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(config.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 // Models
 const { User } = require('./models/user')
@@ -19,6 +19,76 @@ const { Book } = require('./models/book')
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+
+// GET //
+app.get('/api/book', (req,res) => {
+    
+    let id = req.query.id
+
+    Book.findById(id, (err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).send(doc)
+    })
+})
+
+app.get('/api/book/all', (req, res) => {
+
+    const skip = parseInt(req.query.skip)
+    const limit = parseInt(req.query.limit)
+    const order = req.query.order
+
+    Book.find().skip(skip).sort({_id:order}).limit(limit).exec((err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).send(doc)
+    })
+})
+
+// POST //
+
+app.post('/api/book', (req, res)=> {
+    
+    const book = new Book(req.body)
+
+    book.save((err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).json({
+            post: true,
+            bookId: doc._id
+        })
+    })
+
+})
+
+// UPDATE //
+
+app.post('/api/book/update', (req,res) => {
+
+    const id = req.body._id
+    const doc = req.body
+
+    Book.findByIdAndUpdate(id, doc, { new: true, useFindAndModify: false }, (err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).json({
+            success: true,
+            doc
+        })
+    })
+})
+
+// DELETE //
+
+app.delete('/api/book/delete', (req,res) => {
+
+    const id = req.query.id
+
+    Book.findByIdAndRemove(id, (err, doc) => {
+        if(err) return res.status(400).send(err)
+        res.status(200).json({
+            success: true
+        })
+    })
+    
+})
 
 
 const port = process.env.PORT || 3001
